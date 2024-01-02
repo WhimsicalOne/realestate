389498c422ece1de5bd47c7bb7cbb9e3a7080464
Dean LD <dean@local-design.co.za>
2024-01-03 00:08:44 +0200

    Added: Seeding default data when database refreshes completely.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Role;
+use App\Models\Type;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,12 +16,80 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $categories = array(
+            1 => array(
+                'name' => 'Residential',
+                'slug' => 'residential',
+            ),
+            2 => array(
+                'name' => 'Land',
+                'slug' => 'land',
+            ),
+            3 => array(
+                'name' => 'Commercial',
+                'slug' => 'commercial',
+            ),
+            4 => array(
+                'name' => 'Industrial',
+                'slug' => 'industrial',
+            ),
+            5 => array(
+                'name' => 'Investment',
+                'slug' => 'investment',
+            ),
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $types = array(
+            1 => array(
+                'name' => 'Rent',
+            ),
+            2 => array(
+                'name' => 'Buy',
+            ),
+            3 => array(
+                'name' => 'Sell',
+            ),
+        );
+
+        $roles = array(
+            1 => array(
+                'name' => 'Standard',
+            ),
+            2 => array(
+                'name' => 'Admin',
+            ),
+            3 => array(
+                'name' => 'Overlord',
+            ),
+        );
+
+
+        foreach($categories as $c) {
+            Category::create([
+                'name' => $c['name'],
+                'slug' => $c['slug'],
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+
+        foreach($types as $t) {
+            Type::create([
+                'name' => $t['name'],
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+
+        foreach($roles as $r) {
+            Role::create([
+                'name' => $r['name'],
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+
+        \App\Models\Listing::factory(10)->create();
 
     }
 }
